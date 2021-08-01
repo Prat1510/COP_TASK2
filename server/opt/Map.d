8b02bc9a6f0171c03ff1fb311f9d3b479265583d
Gunjaniitd <gunjan3723@gmail.com>
2021-08-01 13:39:32 +0530

    cleaned repo

--- server/opt/Map.d
+++
@@ -1,2 +0,0 @@
-opt/Map.o: src/Map.cpp src/enet.h src/Map.hpp src/Basic.hpp \
- src/ImageMaster.hpp
--- server/opt/main.d
+++
@@ -1,3 +0,0 @@
-opt/main.o: src/main.cpp src/enet.h src/CoviWar.hpp src/Man.hpp \
- src/Basic.hpp src/ImageMaster.hpp src/InputMaster.hpp src/Map.hpp \
- src/SoundMaster.hpp src/TextMaster.hpp src/Coin.hpp src/Virus.hpp
